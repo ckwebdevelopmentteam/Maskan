57138c2b91c6d460e6ed4efe0a2ed50ef1da2fed
muhdyasirr <muhdyasir223@gmail.com>
2026-06-17 13:28:11 +0530

    Hide sticky contact buttons on admin routes

--- components/Client/StickyContactButtons.tsx
+++ components/Client/StickyContactButtons.tsx
@@ -3,8 +3,16 @@
 import React from "react";
 import { Phone } from "lucide-react";
 import WhatsappLogo from "../SVGComponents/socials/WhatsappLogo";
+import { usePathname } from "next/navigation";
 
 export const StickyContactButtons = () => {
+  const pathname = usePathname();
+
+  // Hide on admin dashboard
+  if (pathname?.startsWith("/admin")) {
+    return null;
+  }
+
   return (
     <div className="fixed bottom-6 right-6 z-[9999] flex flex-col gap-4">
       {/* WhatsApp Button */}
